5c21157379836691763e310f53d7365715e09996
harvinder23 <singhh62@student.douglascollege.ca>
2020-02-14 14:21:32 -0800

    name and email added.

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Harvinder Singh");
+		System.out.println("singhh62@student.douglascollege.ca");
+		
+	}
+
+}
